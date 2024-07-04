0399010ab13a42e885d356b3f4e1dd4fca11fddf
Martín Burgos <martinjburgosa@gmail.com>
2024-07-04 10:14:23 -0500

    [Mb] - Ajuste en http client

--- lib/src/datasource/http/http_client_impl.dart
+++ lib/src/datasource/http/http_client_impl.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+import '../../common/types.dart';
 import '../album_api/dto/photo_ds_dto.dart';
+import '../common/failures/datasource_failures.dart';
 import 'http_client.dart';
 
 class AlbumHttpClientImpl implements AlbumHttpClient {
@@ -9,7 +11,7 @@ class AlbumHttpClientImpl implements AlbumHttpClient {
   final http.Client client;
 
   @override
-  Future<dynamic> request({
+  FutureResult<dynamic> request({
     required String url,
     Map<String, dynamic>? body,
     Map<String, String>? headers,
@@ -28,25 +30,38 @@ class AlbumHttpClientImpl implements AlbumHttpClient {
       );
       return _handleRespose(response, isListRequest: isListRequest);
     } catch (e) {
-      throw Exception();
+      return ResultExt.failure(OtherDatasourceFailure(e.toString()));
     }
   }
 
-  Future<dynamic> _handleRespose(
+  FutureResult<dynamic> _handleRespose(
     http.Response response, {
     bool isListRequest = false,
   }) async {
-    if (response.statusCode >= 200 && response.statusCode < 300) {
+    if (response.statusCode == 200) {
       final jsonData = jsonDecode(response.body);
       try {
         if (isListRequest) {
-          return PhotoDsDto.fromJsonList(jsonData);
+          return ResultExt.value(PhotoDsDto.fromJsonList(jsonData));
         } else {
-          return PhotoDsDto.fromJson(jsonData);
+          return ResultExt.value(PhotoDsDto.fromJson(jsonData));
         }
       } catch (e) {
-        throw Exception();
+        return ResultExt.failure(
+          OtherDatasourceFailure(e.toString()),
+        );
       }
+    } else if (response.statusCode == 404) {
+      return ResultExt.failure(const NotFoundDatasourceFailure());
+    } else if (response.statusCode == 500) {
+      return ResultExt.failure(const InternalServerDatasourceFailure());
+    } else if (response.statusCode == 401) {
+      return ResultExt.failure(
+        UnauthorizedDatasourceFailure(response.body),
+      );
+    } else {
+      return ResultExt.failure(
+          const OtherDatasourceFailure('Unknown service error'));
     }
   }
 }
