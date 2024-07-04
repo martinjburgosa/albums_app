97b7f2ea36e6ccc867ea91b39ce549a0d3fef7d2
Martín Burgos <martinjburgosa@gmail.com>
2024-07-04 16:29:45 -0500

    [Mb] - Se agregan labels de failures a archivos de internacionalización

--- lib/src/presentation/config/translation/langs/en_us.dart
+++ lib/src/presentation/config/translation/langs/en_us.dart
@@ -4,5 +4,9 @@ const Map<String, String> enUs = {
   'lbl_home_total_elements': 'Total elements',
   'lbl_detail_album_id': 'Album ID: ',
   'lbl_detail_photo_id': 'Photo ID: ',
-  'lbl_error_title': 'Something went wrong!'
+  'lbl_error_title': 'Something went wrong!',
+  'lbl_failure_101': 'The requested resource was not found',
+  'lbl_failure_102': 'Internal server error',
+  'lbl_failure_103': 'You are not authorized to access this resource',
+  'lbl_failure_104': 'Unknown error in the remote service',
 };
--- lib/src/presentation/config/translation/langs/es_co.dart
+++ lib/src/presentation/config/translation/langs/es_co.dart
@@ -4,5 +4,9 @@ const Map<String, String> esCo = {
   'lbl_home_total_elements': 'Total de elementos',
   'lbl_detail_album_id': 'ID del álbum: ',
   'lbl_detail_photo_id': 'ID de la foto: ',
-  'lbl_error_title': '¡Algo salió mal!'
+  'lbl_error_title': '¡Algo salió mal!',
+  'lbl_failure_101': 'No se ha encontrado el recurso solicitado',
+  'lbl_failure_102': 'Error interno del servidor',
+  'lbl_failure_103': 'No está autorizado para acceder a este recurso',
+  'lbl_failure_104': 'Error desconocido en el servicio remoto',
 };
